d6e2ceed3cf3e08aad4fdb04e5d95f1e44ddd77a
Jacob Marks <jacobjmarks@outlook.com>
2021-05-28 23:45:07 +1000

    Define basic Ping test suite

---
+++ src/Ping.spec.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { mocked } from "ts-jest/utils";
+import { UpApiInterface } from ".";
+
+jest.mock("axios");
+
+const mockedAxios = mocked(axios, true);
+mockedAxios.create.mockImplementation(() => mockedAxios);
+
+describe("Ping", () => {
+  let up: UpApiInterface | null = null;
+
+  beforeEach(() => {
+    up = new UpApiInterface("stub");
+  });
+
+  test("200 OK", async () => {
+    mockedAxios.get.mockResolvedValueOnce({ status: 200 });
+    const successful = await up?.ping();
+    expect(successful).toBe(true);
+  });
+
+  test("401 Not Authorized", async () => {
+    mockedAxios.get.mockResolvedValueOnce({ status: 401 });
+    const successful = await up?.ping();
+    expect(successful).toBe(false);
+  });
+});
