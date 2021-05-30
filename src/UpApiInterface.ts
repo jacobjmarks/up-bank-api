324d253dfa314249976969b333b670c94edbd34c
Jacob Marks <jacobjmarks@outlook.com>
2021-05-30 19:29:00 +1000

    Define exhaustive account/transaction getters

--- src/UpApiInterface.ts
+++ src/UpApiInterface.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import { UP_API_BASEURL } from "./config";
-import { getAccount, getAccounts } from "./modules/accounts";
-import { getTransactions } from "./modules/transactions";
+import { Account, getAccount, getAccounts } from "./modules/accounts";
+import { getTransactions, Transaction } from "./modules/transactions";
 import { ping } from "./modules/utility";
 
 export class UpApiInterface {
@@ -24,4 +24,22 @@ export class UpApiInterface {
   public getAccounts = getAccounts;
   public getAccount = getAccount;
   public getTransactions = getTransactions;
+
+  /**
+   * Get all accounts.
+   * @warning This is a custom exhaustive fetch that may result in multiple API calls
+   * @returns All accounts
+   */
+  public async getAllAccounts(): Promise<Account[]> {
+    return await (await this.getAccounts()).getRemainingData();
+  }
+
+  /**
+   * Get all transactions.
+   * @warning This is a custom exhaustive fetch that may result in multiple API calls
+   * @returns All transactions
+   */
+  public async getAllTransactions(): Promise<Transaction[]> {
+    return await (await this.getTransactions()).getRemainingData();
+  }
 }
